aceae46eb63e3c92b84d94dadf42740e6cdbb1f3
francisZC <31509698@qq.com>
2022-03-22 14:54:26 +0800

    feat: add pm2 deploy config

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    apps : [{
+      name: 'demeter-api-server',
+      script: 'app.js',
+      watch: false,
+      env:{
+        NODE_ENV: 'development',
+  
+      },
+      env_production:{
+        NODE_ENV: 'production'
+      }
+  
+    }
+  ],
+  
+    deploy : {
+      production : {
+        user : 'root',
+        host : '121.5.15.192',
+        ref  : 'origin/main',
+        repo : 'https://github.com/francisZC/Demeter_server.git',
+        path : '/var/www/production',
+        'pre-deploy-local': '',
+        'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+        'pre-setup': ''
+      }
+    }
+};
+  
